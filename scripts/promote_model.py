318a1c5b559a488662dfebe75f47ae99d315797a
Shakeer Mohammed <mds.shakeer@gmail.com>
2026-06-29 13:15:38 +0530

    ci10

--- scripts/promote_model.py
+++ scripts/promote_model.py
@@ -21,7 +21,7 @@ def promote_model():
 
     client = mlflow.MlflowClient()
 
-    model_name = "my_model"
+    model_name = "sentiment_model"
     # Get the latest version in staging
     latest_version_staging = client.get_latest_versions(model_name, stages=["Staging"])[0].version
 
